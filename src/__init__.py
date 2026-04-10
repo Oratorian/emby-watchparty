36d68e009ae4a88d682c8932249e2d974133ba52
Oratorian <ogamenotify@gmail.com>
2026-04-10 16:08:14 +0200

    Bump version to 1.6.1

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 """
 Emby Watch Party - Source Modules
-Version: 1.6.0
+Version: 1.6.1
 Codename: Synced Popcorn
 """
 
-__version__ = "1.6.0"
+__version__ = "1.6.1"
 __codename__ = "Synced Popcorn"
