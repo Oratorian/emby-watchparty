609b26c1c5e882448c996233494f77c4f33e27c7
Oratorian <ogamenotify@gmail.com>
2026-09-11 21:05:40 +0200

    chore: update version to 3.0.0-beta4 in project files

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,8 +1,8 @@
 """
 Emby Watch Party - Source Modules
-Version: 3.0.0-beta3
+Version: 3.0.0-beta4
 Codename: Director's Cut
 """
 
-__version__ = "3.0.0-beta3"
+__version__ = "3.0.0-beta4"
 __codename__ = "Director's Cut"
